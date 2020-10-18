73d654e080737375d002ddb6e35553fb05b5f5a9
Your Name <you@example.com>
2020-10-17 22:47:24 -0700

    Updates

---
+++ precache-manifest.94d673248e03b13c29b12f5826682f85.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fa5917ec75d0a72cd503867806142ee7",
+    "url": "/index.html"
+  },
+  {
+    "revision": "8f9b6146bcd8656f73d5",
+    "url": "/static/css/2.2a86faee.chunk.css"
+  },
+  {
+    "revision": "0c6cc70ac204894e042d",
+    "url": "/static/css/main.0197e277.chunk.css"
+  },
+  {
+    "revision": "8f9b6146bcd8656f73d5",
+    "url": "/static/js/2.7835e9ff.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.7835e9ff.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0c6cc70ac204894e042d",
+    "url": "/static/js/main.160f0035.chunk.js"
+  },
+  {
+    "revision": "dd8147bbe7734801bdde",
+    "url": "/static/js/runtime-main.e312f208.js"
+  },
+  {
+    "revision": "f86c24af59d080b6019271c4c067fa83",
+    "url": "/static/media/BasketballQuestion.jpg.f86c24af.png"
+  },
+  {
+    "revision": "2452d31dc7d4218c8ad992312b8cb96f",
+    "url": "/static/media/BasketballStat.jpg.2452d31d.png"
+  },
+  {
+    "revision": "c75df1f6ac15da7a9eb442c27dc6b846",
+    "url": "/static/media/Employee.jpg.c75df1f6.png"
+  },
+  {
+    "revision": "328965d3fc35c74bd143ebf9fa654754",
+    "url": "/static/media/FitnessTracker.jpg.328965d3.png"
+  },
+  {
+    "revision": "9963ea21ec60dedfeeb946686626ba25",
+    "url": "/static/media/Screenshot (8).9963ea21.png"
+  },
+  {
+    "revision": "837a68a71c564efc75703fb03bcebc6d",
+    "url": "/static/media/teo.837a68a7.jpg"
+  }
+]);
